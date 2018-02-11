1d2da164221e7b2a853e6859d5a1b5f3cdb84f58
Brandon Cooke <bcooke91@gmail.com>
2018-02-11 22:52:24 +0000

    nuking progress

--- config/express.config.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-let express = require('express');
-let bp      = require('body-parser');
-let server  = express();
-server.use(bp.json());
-server.use(bp.urlencoded());
-
-module.exports = server;
