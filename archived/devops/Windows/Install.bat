c99aec06b9397bd0d54f6944e9c91c53beae1e64
Brandon Cooke <bcooke91@gmail.com>
2018-02-12 19:19:17 +0000

    Nuking again

--- archived/devops/Windows/Install.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-REM BATCH WRAPPER FOR INSTALL POWERSHELL SCRIPT
-PowerShell.exe -ExecutionPolicy Bypass -File .\Install.ps1
--- archived/devops/Windows/RunServer.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-REM BATCH WRAPPER FOR RUN SERVER POWERSHELL SCRIPT
-PowerShell.exe -ExecutionPolicy Bypass -File .\RunServer.ps1
